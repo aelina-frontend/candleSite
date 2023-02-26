073f22d9b94d4d57f96b0f9ad1510b35e1486641
drakkonne <gagind@blastmaker.kg>
2023-02-26 21:18:43 +0600

    Сделали кнопки в меню

--- app.js
+++ app.js
@@ -1,33 +1,16 @@
 'use strict';
-
-const switcher = document.querySelector('.btn');
-
-// switcher.addEventListener('click', function() {
-//     document.body.classList.toggle('light-theme');
-//     document.body.classList.toggle('dark-theme');
-
-//         const className = document.body.className;
-//     if(className == "light-theme") {
-//         this.textContent = "dark";
-//     } else {
-//         this.textContent = "light";
-//     }
-//     console.log('current class name: ' + className);
-// });
-
-// function openCity(evt,cityName) {
-//     var i,tabcontent, tablinks;
-//     tabcontent = document.getElementsByClassName("tabcontent");
-//     for (i = 0; i < tabcontent.length; i++) {
-//         tabcontent[i].style.display = "none";
-//     }
-//     tablinks = document.getElementsByClassName("tablinks");
-//     for (i = 0; i < tablinks.length; i++) {
-//         tablinks[i].className = tablinks[i].className.replace("active", "");
-//     }
-//     document.getElementById(cityName).style.display = "block";
-//     evt.currentTarget.className += "active";
-// }
-
-
-
+window.addEventListener("load", function() {
+var switcher = document.querySelector('#btn');
+switcher.addEventListener('click', function() {    
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+    console.log("dfdfd");
+    const className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "dark";
+    } else {
+        this.textContent = "light";
+    }
+    console.log('current class name: ' + className);
+});
+});
